c4ebe1d9675f59ded8871a95e8a1485d8fa15a38
“Peehal” <sinhapeehal@gmail.com>
2026-02-25 17:44:33 +0530

    Set up Parcel bundler and configured project structure

--- App.js
+++ App.js
@@ -12,14 +12,20 @@
     </div>
 </div> */}
 
+import React from "react";
+import ReactDOM from "react-dom/client"; 
 
-const parent = React.createElement("div", {id : "parent"}, 
-    React.createElement("div", {id: "child"}, 
-        [React.createElement("h1", {}, "Iam a h1 tag"), 
-            React.createElement("h2", {}, "Iam a h2 tag")
-        ]
-    )
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    [
+      React.createElement("h1", { key: "h1" }, "I am a h1 tag"),
+      React.createElement("h2", { key: "h2" }, "I am a h2 tag")
+    ]
+  )
 );
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
