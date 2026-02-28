0c84f77ee301f366c1373e74d86bfe25a4eecec1
“Peehal” <sinhapeehal@gmail.com>
2026-02-28 15:51:27 +0530

    Added Top Rated filter using useState in Body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
+
 
 const Body = () => {
+
+    let [listOfRestaurants, setListOfRestaurant] = useState(resList);
+
+
   return (
+
     <div className="body">
       <div className="search">
-        <input type="text" placeholder="Search for restaurants..." />
-        <button>Search</button>
+        
+        <button className="filter-btn" 
+        
+        onClick={() => {
+            const filteredRestaurant = listOfRestaurants.filter((res) => res.avg_rating > 4 );
+            setListOfRestaurant(filteredRestaurant);
+        }
+    }
+
+        >Top Rated Restaurants</button>
+
       </div>
 
       <div className="res-container">
         {
-            resList.map((restaurant) => (
+            listOfRestaurants.map((restaurant) => (
                 <RestaurantCard
                 key={restaurant.restaurant_id}
                 resData={restaurant}
