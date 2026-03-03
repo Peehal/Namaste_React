8eff53b7fb6f19058a238063d4d5f9174555404e
“Peehal” <sinhapeehal@gmail.com>
2026-03-03 10:49:59 +0530

    Integrated real-time Swiggy production API for restaurant listing

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,69 +2,78 @@ import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 
-
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
 
-    const [listOfRestaurants, setListOfRestaurant] = useState([]);
+  const [allrestaurant, setallrestaurant] = useState([]);
 
-    const[allrestaurant, setallrestaurant] = useState([]);
+  const [searchText, setsearchText] = useState("");
 
-    const [searchText, setsearchText] = useState("");
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://corsproxy.io/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D22.79296%26lng%3D86.1790513%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING",
+    );
+    const jsonData = await data.json();
 
-    const fetchData = async () => {
-        const data = await fetch("http://localhost:3001/restaurants");
-        const jsonData = await data.json();
+    const restaurants =
+     jsonData?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
+    ?.restaurants;
 
-        console.log(jsonData);
+    setListOfRestaurant(restaurants);
+    setallrestaurant(restaurants);
+  };
 
-        setListOfRestaurant(jsonData);
-        setallrestaurant(jsonData)
-    };
-
-  return listOfRestaurants.length == 0 ? <Shimmer /> : (
-
+  return listOfRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="search">
+        <input
+          type="text"
+          className="search-box"
+          placeholder="Cafe..."
+          value={searchText}
+          onChange={(e) => {
+            setsearchText(e.target.value);
+          }}
+        ></input>
+        <button
+          onClick={() => {
+            const filteredRestaurant = listOfRestaurants.filter((res) =>
+              res?.info?.name?.toLowerCase().includes(searchText.toLowerCase()),
+            );
 
-        <input type="text" className="search-box" placeholder="Cafe..." value={searchText} onChange={(e) => {
-          setsearchText(e.target.value);
-        }}></input>
-        <button  
-        
-        onClick={() => {
-          const filteredRestaurant = listOfRestaurants.filter((res) => res.name.toLowerCase().includes(searchText.toLowerCase()) );
-
-          setallrestaurant(filteredRestaurant);
-
-        }}>Search
+            setallrestaurant(filteredRestaurant);
+          }}
+        >
+          Search
         </button>
-        
-        <button className="filter-btn" 
-        
-        onClick={() => {
-            const filteredRestaurant = listOfRestaurants.filter((res) => res.avg_rating > 4 );
-            setListOfRestaurant(filteredRestaurant);
-        }
-    }
 
-        >Top Rated Restaurants</button>
-
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRestaurant = listOfRestaurants.filter(
+              (res) => res?.info?.avgRating > 4,
+            );
+            setallrestaurant(filteredRestaurant);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
 
       <div className="res-container">
-        {
-            allrestaurant.map((restaurant) => (
-                <RestaurantCard
-                key={restaurant.restaurant_id}
-                resData={restaurant}
-                />
-            ))
-        }
-
+        {Array.isArray(allrestaurant) &&
+          allrestaurant.map((restaurant) => (
+            <RestaurantCard
+              key={restaurant?.info?.id}
+              resData={restaurant?.info}
+            />
+          ))}
       </div>
     </div>
   );
