4fbafafebaa291c90fda171cee597ec588bf566b
“Peehal” <sinhapeehal@gmail.com>
2026-04-08 08:42:37 +0530

    fix: add toggle functionality to restaurant accordion

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -37,7 +37,7 @@ const RestaurantMenu = () => {
                     key={category?.card?.card?.title}
                     data={category?.card?.card}
                     showItems={index === showIndex}
-                    setShowIndex={() => setShowIndex(index)}
+                    setShowIndex={() => setShowIndex(showIndex === index ? null : index)}
                 />
             ))}
         </div>
