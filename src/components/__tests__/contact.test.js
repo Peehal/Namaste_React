359e30c0cda50f5875c4f9864aea84b457c56006
“Peehal” <sinhapeehal@gmail.com>
2026-04-16 20:18:29 +0530

    chore: remove coverage folder from git tracking

--- src/components/__tests__/contact.test.js
+++ src/components/__tests__/contact.test.js
@@ -2,22 +2,38 @@ import { render, screen } from "@testing-library/react"
 import Contact from "../Contact"
 import "@testing-library/jest-dom"
 
-test("Testing the Contact Component first by rendering it", () => {
-    render( <Contact /> );
 
-    const Heading = screen.getByRole("heading");
+describe("Grouping all the conatct test cases", () => {
 
-    expect(Heading).toBeInTheDocument();
+    test("Testing the Contact Component first by rendering it", () => {
+        render(<Contact />);
 
-})
+        const Heading = screen.getByRole("heading");
+
+        expect(Heading).toBeInTheDocument();
+
+    })
+
+    test("Testing the buttol Component first by rendering it", () => {
+        render(<Contact />);
+
+        const button = screen.getByText("Submit");
+
+        expect(button).toBeInTheDocument();
+
+    })
+
+    test("Testing the input name first by rendering it", () => {
+        render(<Contact />);
+
+        const input = screen.getByPlaceholderText("Your Name");
+
+        expect(input).toBeInTheDocument();
+
+    })
+
+});
 
 
 
-test("Testing the buttol Component first by rendering it", () => {
-    render( <Contact /> );
 
-    const button = screen.getByRole("button");
-
-    expect(button).toBeInTheDocument();
-
-})
